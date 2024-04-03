eb44c3e4e93bc94bbc8eecce172c38a96e916cc4
Nahuel Anselmo <anselmonahuel63t@gmail.com>
2024-04-03 20:25:11 -0300

    ejercicio 13 ya realizado

---
+++ js/ejercicio-13.js
@@ -0,0 +1,8 @@
+/*Realiza un script que pida un texto y lo muestre en 
+mayúsculas */
+
+const texto = prompt("Por favor, introduce un texto:");
+
+const textoEnMayusculas = texto.toUpperCase();
+
+document.write("El texto en mayúsculas es: " + textoEnMayusculas);
